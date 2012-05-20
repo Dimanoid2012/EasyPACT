16c1006222fee5225e97084dccf0b580e97797bc
Dmitriy Vilshanskiy <dimanoid2007@gmail.com>
2012-05-21 01:22:31 +0400

    Расширение функционала для работа с БД

--- EasyPACT/Database.cs
+++ EasyPACT/Database.cs
@@ -11,7 +11,7 @@ namespace EasyPACT
     {
         private const string ConnectionString = "Data Source=|DataDirectory|\\EasyPACT.sdf";
         static private readonly SqlCeConnection Connect = new SqlCeConnection(ConnectionString);
-        static public void Open()
+        static private void Open()
         {
             try
             {
@@ -22,21 +22,39 @@ namespace EasyPACT
                 throw new InvalidDatabaseConnectionOpenException("Не получается открыть файл базы данных!");
             }
         }
-        /*static public SqlCeDataReader Query2(string query)
-        {
-            var command = new SqlCeCommand(query, Connect);
-            var reader = command.ExecuteReader();
-            reader.Read();
-            return reader;
-        }*/
         static public List<List<string>> Query(string query)
         {
+            var ret = new List<List<string>>();
+            var command = new SqlCeCommand();
+            command.Connection = Connect;
+            command.CommandType = CommandType.Text;
+            command.CommandText = query;
             Open();
-            var command = new SqlCeCommand(query, Connect);
-            var adapter = new SqlCeDataAdapter(command);
-            return ProcessingQuery(adapter);
+            switch(query.Split(' ')[0].ToLower())
+            {
+                case "select":
+                    var adapter = new SqlCeDataAdapter(command);
+                    ret = Select(adapter);
+                    return ret;
+                case "insert":
+                    if (Insert(command))
+                    {
+                        ret.Add(new List<string>());
+                        ret[0].Add("1");
+                    }
+                    else
+                    {
+                        ret.Add(new List<string>());
+                        ret[0].Add("0");
+                    }
+                    return ret;
+                default:
+                    ret.Add(new List<string>());
+                    ret[0].Add("0");
+                    return ret;
+            }
         }
-        static private List<List<string>> ProcessingQuery(SqlCeDataAdapter adapter)
+        /*static private List<List<string>> ProcessingQuery(SqlCeDataAdapter adapter)
         {
             var dataSet = new DataSet("answer");
             adapter.Fill(dataSet);
@@ -51,23 +69,30 @@ namespace EasyPACT
                 for (int i = 0; i < element.Nodes().Count(); i++)
                     list[i].Add(element.Elements().Nodes().ToArray()[i].ToString());
             return list;
-        }
-        /*static public string Select(string table, string fields, string conditions)
-        {
-            var query = string.Format("SELECT {0} FROM {1} WHERE {2}", fields, table, conditions);
-            Open();
-            SqlCeDataReader ans;
-            try
-            {
-                ans = Query(query);
-            }
-            catch
-            {
-                throw new InvalidDatabaseQueryException("Некорректный запрос к базе данных!");
-            }
-            return "asd";
         }*/
-        static public void Close()
+        static private List<List<string>> Select(SqlCeDataAdapter adapter)
+        {
+            var dataSet = new DataSet("answer");
+            adapter.Fill(dataSet);
+            //dataSet.Container.Add();
+            Close();
+            var list = new List<List<string>>();
+            var schema = XDocument.Parse(dataSet.GetXml());
+            var elements = schema.Root.Elements("Table");
+            var listCount = elements.First().Nodes().Count();
+            for (int i = 0; i < listCount; i++)
+                list.Add(new List<string>());
+            foreach (var element in elements)
+                for (int i = 0; i < element.Nodes().Count(); i++)
+                    list[i].Add(element.Elements().Nodes().ToArray()[i].ToString());
+            return list;
+        }
+        static private bool Insert(SqlCeCommand command)
+        {
+            command.ExecuteNonQuery();
+            return true;
+        }
+        static private void Close()
         {
             Connect.Close();
         }
--- EasyPACT/Program.cs
+++ EasyPACT/Program.cs
@@ -13,15 +13,8 @@ namespace EasyPACT
             System.Threading.Thread.CurrentThread.CurrentCulture = inf;
             inf.NumberFormat.NumberDecimalSeparator = ".";
             // Вывод курсовой
-            var liq = new LiquidMix("6,7", 0.28, 20, 760);
-            var pip = new PipelineRound(33, 1, 0.083, 0.003, 8.1);
-            var vacLip = new LiquidInPipeline(liq, pip);
-            pip = new PipelineRound(33, 1, 0.069, 0.003, 50);
-            var forLip = new LiquidInPipeline(liq, pip);
-            var pump = new CentrifugalPump(37);
-            var network = Network.Create(vacLip, forLip, pump);
-            network.Pump.Run(20.0928*60);
-            Console.WriteLine(network.Productivity*liq.Density*3600/1000);
+            //var ans = Database.Query("insert into liquid_list(name,formula,molar_mass) values('Бутан','C4H10',58.1)");
+            var ans = Database.Query("select * from liquid_list");
             Console.ReadKey();
         }
     }
