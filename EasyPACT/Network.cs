9f32eb898e72972357a47876faa19606173b87ae
Dmitriy Vilshanskiy <dimanoid2007@gmail.com>
2012-05-27 21:03:25 +0400

    Все вроде работает

--- EasyPACT/Network.cs
+++ EasyPACT/Network.cs
@@ -60,6 +60,15 @@ namespace EasyPACT
             _isExist = true;
             return network;
         }
+        public static Network Create(LiquidInPipeline vacuumLine, LiquidInPipeline forcingLine)
+        {
+            if (_isExist)
+                return _network;
+            var network = new Network {VacuumLine = vacuumLine, ForcingLine = forcingLine};
+            _network = network;
+            _isExist = true;
+            return network;
+        }
         /// <summary>
         /// Возвращает объект сети, если он уже создан, либо null, если его нет.
         /// </summary>
@@ -71,13 +80,14 @@ namespace EasyPACT
         /// <summary>
         /// Устанавливает производительность сети. Вызвать может только насос, установленный в сети.
         /// </summary>
-        /// <param name="pump">Насос.</param>
         /// <param name="productivity">Новая производительность.</param>
-        public bool SetProductivity(CentrifugalPump pump, double productivity)
+        public bool SetProductivity(double productivity)
         {
-            if (pump != this.Pump)
-                return false;
             this.Productivity = productivity;
+            if (this.Pump != null)
+                this.Pump.SetProductivity(productivity/this.VacuumLine.Liquid.Density);
+            this.VacuumLine.MassFlow = productivity;
+            this.ForcingLine.MassFlow = productivity;
             return true;
         }
         public bool AddHeatExchanger(HeatExchangerPipe he)
@@ -85,6 +95,31 @@ namespace EasyPACT
             this.HeatExchanger = he;
             this.ForcingLine.Pipeline.AddLocalResistance(2*1.5 + he.NumberOfPipes + 2.5*(he.NumberOfCourses - 1));
             return true;
+        }
+        /// <summary>
+        /// Подобрать насос к заданной сети.
+        /// </summary>
+        /// <param name="massFlow">Необходимый массовый расход.</param>
+        /// <param name="liftingHeight">Высота подъема жидкости.</param>
+        public void ChooseCentrifugalPump(double massFlow, double liftingHeight)
+        {
+            var Hvac = (this.VacuumLine.LossOfPressureUponAFriction() +
+                        this.VacuumLine.LossOfPressureUponLocalResistances())/this.VacuumLine.Liquid.Density/9.81;
+            var Hfor = (this.ForcingLine.LossOfPressureUponAFriction() +
+                        this.ForcingLine.LossOfPressureUponLocalResistances()) / this.ForcingLine.Liquid.Density / 9.81;
+            var Hhe = (this.HeatExchanger.LiquidInCase.LossOfPressureUponAFriction() +
+                       this.HeatExchanger.LiquidInCase.LossOfPressureUponLocalResistances())/
+                      this.HeatExchanger.LiquidInCase.Liquid.Density/9.81;
+            var H = Hvac + Hfor + Hhe + liftingHeight;
+            var N = massFlow*9.81*H/0.6;
+            var list =
+                Database.Query(
+                    String.Format("SELECT id FROM centrifugal_pumps WHERE productivity>={0} AND pressure>={1}",
+                                  massFlow/this.VacuumLine.Liquid.Density, H));
+            var id = list[0].First();
+            this.Pump = new CentrifugalPump(Convert.ToInt32(id));
+            this.SetProductivity(massFlow);
+
         }
         /// <summary>
         /// Подобрать теплообменник к заданной сети.
@@ -106,27 +141,62 @@ namespace EasyPACT
             var steam = new LiquidPure("10", temperatureSteam, Calculation.Pressure(10, temperatureSteam));
             steam.Evaporate();
             liq.Temperature = t;
-            this.Productivity = 32 * 1000 / 3600 / liq.Density; // УДАЛИТЬ
-            var Q = this.Productivity*liq.Density*liq.ThermalCapacity/1000*
+            //this.Productivity = 32 * 1000 / 3600 / liq.Density; // УДАЛИТЬ
+            var Q = this.Productivity*liq.ThermalCapacity/1000*
                     (temperatureLiquid - this.ForcingLine.Liquid.Temperature);
             var G1 = Q/steam.VaporizationHeat;
             var V1 = G1/steam.Density;
-            var V2 = this.Productivity;
+            var V2 = this.Productivity/liq.Density;
             var dtMax = steam.Temperature - this.ForcingLine.Liquid.Temperature;
             var dtMin = steam.Temperature - temperatureLiquid;
-            var dt = (dtMax - dtMin)/Math.Log(dtMax - dtMin);
+            var dt = (dtMax - dtMin)/Math.Log(dtMax/dtMin);
             var Kor = 120;
             var For = Q*1000/Kor/dt;
             var Re2min = 10000;
             var w2min = Re2min*liq.ViscosityDynamic/liq.Density/0.021;
-            var nmax = V2/0.785/0.021/0.021/w2min;
+            var nmax = Math.Ceiling(V2/0.785/0.021/0.021/w2min);
             var list =
                 Database.Query(string.Format("SELECT id FROM XXXIV WHERE surface_area < {0} AND pipes_per_course < {1}",
                                              For, nmax))[0];
-            var id = list.Last();
+            var id = list.First();
             var he = new HeatExchangerPipe(Convert.ToInt32(id));
             he.SetLiquidInCase(steam);
             he.SetLiquidInPipes(this.ForcingLine.Liquid);
+            double F = 0;
+            while(true )
+            {
+                var Re2 = Re2min*(nmax*he.NumberOfCourses/he.NumberOfPipes);
+                var Pr2 = liq.Pr;
+                var Nu2 = 0.021*Math.Pow(Re2, 0.8)*Math.Pow(Pr2, 0.43);
+                var a2 = Nu2*liq.ThermalConductivity/0.021;
+                var q = Q/he.SurfaceArea;
+                steam.Condence();
+                var a1 = 1.21*steam.ThermalConductivity*
+                         Math.Pow(
+                             Math.Pow(steam.Density, 2)*steam.VaporizationHeat*9.81/steam.ViscosityDynamic/
+                             he.PipesLength, 0.33)*Math.Pow(q, -0.33);
+                var d = 0.002;
+                var r = d/he.Pipeline.ThermalConductivity;
+                var r1 = 1/1600.0;
+                var r2 = 1/5800.0;
+                r = r + r1 + r2;
+                var K = 1/(1/a1 + r + 1/a2);
+                F = Q*1000/K/dt;
+                var zap = (he.SurfaceArea - F)/he.SurfaceArea;
+                if((zap < 0.1 | zap > 0.3) & list.Count > 0)
+                {
+                    list.RemoveAt(0);
+                    id = list.First();
+                    he = new HeatExchangerPipe(Convert.ToInt32(id));
+                    he.SetLiquidInCase(steam);
+                    he.SetLiquidInPipes(this.ForcingLine.Liquid);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            he.LiquidInCase.MassFlow = G1;
             this.HeatExchanger = he;
 
         }
